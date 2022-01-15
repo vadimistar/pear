34c2bdb285095b2741ed5ed19b6c4633986e583c
Vadimistar <vadimistar@ya.ru>
2022-01-15 16:22:28 +0700

    vm now interprets bytecode files

--- pearvm/src/main.rs
+++ pearvm/src/main.rs
@@ -2,10 +2,36 @@ mod vm;
 
 use vm::*;
 
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+};
+
 fn main() {
-    let mut vm = VM::new(vec![1, 2, 0, 3, 4]);
-    match vm.run() {
-        Ok(()) => {},
-        Err(err) => eprintln!("Error: {}", err)
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.len() {
+        2 => {
+            if !args[1].ends_with(".prbc") {
+                panic!(
+                    "Error: Invalid input file extension: {} (expected .prbc)",
+                    args[1]
+                );
+            }
+
+            let file = File::open(&args[1]).expect("Can not read the input file");
+            let mut reader = BufReader::new(file);
+            let mut buffer = Vec::new();
+
+            reader.read_to_end(&mut buffer).expect("Can't read file");
+
+            let mut vm = VM::new(buffer);
+
+            match vm.run() {
+                Ok(()) => {}
+                Err(err) => eprintln!("Error: {}", err),
+            }
+        }
+        _ => todo!(),
     }
 }
